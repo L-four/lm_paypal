01f64ee354a8c0c55b480de3e4a21196f45bf94d
Simon Roberts <lyricnz@33667.no-reply.drupal.org>
2008-10-13 04:24:39 +0000

    #209287 by nekobul, fonant, AlexisWilke and lyricnz: Initial Port to Drupal 6

--- drop_test.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS lm_paypal_ipns;
-DROP TABLE IF EXISTS lm_paypal_subscribers;
-DROP TABLE IF EXISTS lm_paypal_subscriptions;
-DROP TABLE IF EXISTS lm_paypal_donations;
-DROP TABLE IF EXISTS lm_paypal_paid_adverts;
-DELETE FROM system WHERE name = 'lm_paypal';
-DELETE FROM system WHERE name = 'lm_paypal_subscriptions';
-DELETE FROM system WHERE name = 'lm_paypal_donations';
-DELETE FROM system WHERE name = 'lm_paypal_paid_adverts';
